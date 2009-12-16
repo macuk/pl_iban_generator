5c59e9492918141cef557c8f132ece083cb53f17
Piotr Macuk <piotr@macuk.pl>
2009-12-16 11:47:38 +0100

    First implementation

--- lib/pl_iban_generator.rb
+++ lib/pl_iban_generator.rb
@@ -1,1 +1,39 @@
-# PLIbanGenerator
+class PlIbanGenerator
+  class NumberInvalid < Exception; end
+
+  attr_accessor :bank_code, :account_no, :prefix
+
+  def initialize(bank_code, account_no, prefix='PL')
+    self.bank_code = bank_code
+    self.account_no = account_no
+    @prefix = prefix.to_s
+  end
+
+  def bank_code=(bank_code)
+    @bank_code = bank_code.to_s.gsub(/\D/, '')
+    msg = 'Wrong bank code number length!'
+    raise NumberInvalid, msg if @bank_code.size != 8
+  end
+
+  def account_no=(account_no)
+    @account_no = account_no.to_s.gsub(/\D/, '')
+    msg = 'Wrong account number length!'
+    raise NumberInvalid, msg if @account_no.size != 16
+  end
+
+  def get
+    @prefix + check_digits + @bank_code + @account_no
+  end
+
+  def to_s
+    n = get
+    return n.gsub(/(.{4})/, '\1 ').strip if n.size == 28
+    return n[0..1] + ' ' + n[2..-1].gsub(/(.{4})/, '\1 ').strip if n.size == 26
+  end
+
+  private
+    def check_digits
+      n = @bank_code + @account_no + '252100' # A=10, B=11, ..., L=21, P=25, ... + '00'
+      sprintf('%02d', 98 - (n.to_i % 97))
+    end
+end
--- test/pl_iban_generator_test.rb
+++ test/pl_iban_generator_test.rb
@@ -1,8 +1,48 @@
 require 'test_helper'
 
 class PLIbanGeneratorTest < ActiveSupport::TestCase
-  # Replace this with your real tests.
-  test "the truth" do
-    assert true
+  test "changing parameters to strings" do
+    g = PlIbanGenerator.new(12345678, 1234567890123456, 3)
+    assert_equal '12345678', g.bank_code
+    assert_equal '1234567890123456', g.account_no
+    assert_equal '3', g.prefix
+  end
+
+  test "cleaning parameters" do
+    g = PlIbanGenerator.new('1234 5678', '0000 1111 2222 3333')
+    assert_equal '12345678', g.bank_code
+    assert_equal '0000111122223333', g.account_no
+  end
+
+  test "check digits calculation" do
+    g = PlIbanGenerator.new(11402004, '0000 3002 3471 3036', '')
+    assert_equal '47114020040000300234713036', g.get
+    g.account_no = '0000 3302 0772 6785'
+    assert_equal '43114020040000330207726785', g.get
+  end
+
+  test "to_s implementation" do
+    g = PlIbanGenerator.new('1140 2004', '0000 3002 3471 3036', '')
+    assert_equal '47 1140 2004 0000 3002 3471 3036', g.to_s
+    g.prefix = 'PL'
+    assert_equal 'PL47 1140 2004 0000 3002 3471 3036', g.to_s
+  end
+
+  test "check number size exception" do
+    assert_raise PlIbanGenerator::NumberInvalid do
+      g = PlIbanGenerator.new(nil, nil)
+    end
+    assert_raise PlIbanGenerator::NumberInvalid do
+      g = PlIbanGenerator.new('1140 2004 4', '0000 3002 3471 3036', '')
+    end
+    assert_raise PlIbanGenerator::NumberInvalid do
+      g = PlIbanGenerator.new('1140 200', '0000 3002 3471 3036', '')
+    end
+    assert_raise PlIbanGenerator::NumberInvalid do
+      g = PlIbanGenerator.new('1140 2004', '0000 3002 3471 3036 9', '')
+    end
+    assert_raise PlIbanGenerator::NumberInvalid do
+      g = PlIbanGenerator.new('1140 2004', '0000 3002 3471 303', '')
+    end
   end
 end
